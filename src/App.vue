403c8dc0f996c27b445279b8c71afa5b11520004
Jaroslaw Oskroba <jaroslawoskroba@gmail.com>
2023-04-16 14:44:16 +0200

    added logging field

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    <div v-if = "loggedIn == false">Zaloguj się e-mailem 
+      <input type="email" v-model="email">
+      <button @click="wchodze()">Wchodzę</button>
+    </div>
+    <div v-else-if = "loggedIn == true">Witaj {{ email }}
+    <button @click="wyloguj()">Wchodzę</button>
+  </div>
   </div>
 </template>
 
 <script>
+  import "milligram";
 export default {
+
+  data() {
+  return {
+    email: 'j@agh.edu.pl',
+    password: '',
+    loggedIn: false
+  };
+},
+methods: {
+      wchodze() {
+        this.loggedIn = true;
+      },
+      wyloguj() {
+        this.loggedIn = false;
+      }
+  },
 
 }
 </script>
-
 <style>
 .my-app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -17,3 +40,8 @@ export default {
   color: #2c3e50;
 }
 </style>
+<!--     
+    <div v-if="!email.includes('@')">Twój adres email jest niepoprawny</div>
+    <div v-else-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi, ponieważ ma {{email.length }} znaków</div> -->
